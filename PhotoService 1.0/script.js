7e4244e7dbb808508d0a8abb66711e3ec3270640
flame1800 <ruslan08022001@gmail.com>
2019-11-08 10:54:24 +0500

    My PhotoApps for Ws (start project)

---
+++ PhotoService 1.0/script.js
@@ -0,0 +1,89 @@
+
+
+const getData = async (url) => {
+    const fetchResponse = await fetch(url);
+    const result = await fetchResponse.json();
+
+    return result;
+}
+
+
+
+const makePhoto = function(url, gall) {
+    const gallery = document.querySelector(gall);
+    const img = document.createElement('img');
+    const imgArray = []
+    img.classList.add('img');
+    img.src = url;
+    img.draggable = true;
+    imgArray.push = img;
+    gallery.appendChild(img);
+
+    return gallery;
+}
+
+
+// Big Image f()
+const clickImg = function(image) {
+    image.addEventListener('click', () => {
+        image.style.display = 'none';
+    });
+}
+
+//Drag n Drop
+const draggedItem = null;
+
+const dragNdrop = (image) => {
+    image.addEventListener("dragstart", () => {
+        console.log('dragstart');
+        draggedItem = this;
+        image.style.display = 'none';
+    })
+    
+    img.addEventListener("dragend", () => {
+        console.log('dragend');
+       
+    })
+}
+
+
+
+//API
+getData('https://api.unsplash.com/search/photos?query=cats&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12').then((res) => {
+    const photoArray = res.results;
+    const imgArray = []
+
+    for (key in photoArray) {
+        makePhoto(photoArray[key].urls.regular, '.gallery1');
+        const img = document.querySelector('.img')      
+        clickImg(img);
+    }
+    console.log(imgArray);
+    
+
+})
+getData('https://api.unsplash.com/search/photos?query=food&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12').then((res) => {
+    const photoArray = res.results;
+
+    for (key in photoArray) {
+        makePhoto(photoArray[key].urls.regular, '.gallery2')
+    }
+})
+getData('https://api.unsplash.com/search/photos?query=humans&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12').then((res) => {
+
+    const photoArray = res.results;
+
+    for (key in photoArray) {
+        makePhoto(photoArray[key].urls.regular, '.gallery3')
+    }
+})
+getData('https://api.unsplash.com/search/photos?query=cars&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12').then((res) => {
+    console.log(res.results);
+    const photoArray = res.results;
+
+    for (key in photoArray) {
+        makePhoto(photoArray[key].urls.regular, '.gallery4')
+    }
+})
+
+
