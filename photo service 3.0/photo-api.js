f7a7355523b6a757a6f41a38a00599dde7aa2437
flame1800 <ruslan08022001@gmail.com>
2019-11-17 16:26:35 +0300

    functional ph 3.0

--- photo service 3.0/photo-api.js
+++ photo service 3.0/photo-api.js
@@ -4,7 +4,7 @@ const getData = async (url) => {
     const result = await getResponse.json();
 
     return result;
-} 
+}
 const imgArray = [];
 const togglePhoto = document.querySelector('.toggle-photo')
 
@@ -19,45 +19,54 @@ const createImg = (url, container) => {
     return img;
 }
 
+const togglerPhoto = (container) => {
+    const selectPhoto = imgArray[key];
+    selectPhoto.addEventListener('click', () => {
+        togglePhoto.appendChild(selectPhoto);
+        togglePhoto.style.display = 'flex';
+        selectPhoto.style.width = 'auto';
+        selectPhoto.style.height = 'auto';
+    });
+    togglePhoto.addEventListener('click', () => {
+        togglePhoto.style.display = 'none';
+        container.appendChild(selectPhoto);
+        selectPhoto.style.width = '190px';
+        selectPhoto.style.height = '150px';
+    })
+}
+
 const containerOne = document.querySelector('.photo-container1');
-const apiOne = 'https://api.unsplash.com/search/photos?query=blue&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12' 
+const apiOne = 'https://api.unsplash.com/search/photos?query=blue&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12'
 
 const containerTwo = document.querySelector('.photo-container2');
-const apiTwo = 'https://api.unsplash.com/search/photos?query=greeb&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12' 
+const apiTwo = 'https://api.unsplash.com/search/photos?query=greeb&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12'
 
 const containerThree = document.querySelector('.photo-container3');
-const apiThree = 'https://api.unsplash.com/search/photos?query=red&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12' 
+const apiThree = 'https://api.unsplash.com/search/photos?query=red&client_id=156d988dd84249d71254da4f9348f3f7e795037a4d8c43a5a76526856c40cb12'
 
 
 getData(apiOne).then((result) => {
 
-    for(key in result.results) {
-        createImg(result.results[key].urls.small, containerOne)
+    for (key in result.results) {
+        createImg(result.results[key].urls.small, containerOne);
+        togglerPhoto(containerOne);
     }
 
-    for (key in imgArray) {
-        imgArray[key].onclick = function() {
-            togglePhoto.appendChild(imgArray[key]);
-            togglePhoto.style.display = 'flex';
-            imgArray[key].style.width = 'auto';
-            imgArray[key].style.height = '80%';
-        }
-    }
+
 })
 
 getData(apiTwo).then((result) => {
 
-    for(key in result.results) {
-
-        createImg(result.results[key].urls.small, containerTwo)
+    for (key in result.results) {
+        createImg(result.results[key].urls.small, containerTwo);
+        // togglerPhoto(containerTwo);
     }
 })
 
 getData(apiThree).then((result) => {
 
-    for(key in result.results) {
-
-        createImg(result.results[key].urls.small, containerThree)
+    for (key in result.results) {
+        createImg(result.results[key].urls.small, containerThree);
+        // togglerPhoto(containerThree);
     }
 })
-
